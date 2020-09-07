882dfb463f5192d33b00e9821618bed280c276c5
Sajad <sajadkhosravani1@gmail.com>
2020-09-08 01:36:31 +0430

    Models has been initialized.

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+    balance = models.IntegerField()
+    user = models.OneToOneField(User,on_delete=models.CASCADE,)
+
+
+class Product(models.Model):
+    code = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    inventory = models.IntegerField()
+
+
+# TODO : Product class
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    order_time = models.DateTimeField()
+    total_price = models.IntegerField()
+    status = models.IntegerField()  # TODO : choices for status
+    # TODO : rows
+
+
+
+# TODO : OrderRow class
