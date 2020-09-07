c71983db650f46d880a3a0363d0bd1065e58d79a
Sajad <sajadkhosravani1@gmail.com>
2020-09-07 23:08:53 +0430

    MainApp has been installed. Time zone has been changed to Tehran. And language has changed to Persian(Farsi).

--- OrderManagement/settings.py
+++ OrderManagement/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainApp.apps.MainAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-ir'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# has been developed by Sajad Khosravani
+# github.com/sajadkhosravani1
