3589ade4cc6ba049f2fe6b8b31e392568046e863
sajadkhosravani1 <sajadkhosravani1@gmail.com>
2020-09-18 09:11:40 +0430

    changed

--- OrderManagement/urls.py
+++ OrderManagement/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('market/', include('mainApp.urls'))
+    path('', include('mainApp.urls'))
 ]
