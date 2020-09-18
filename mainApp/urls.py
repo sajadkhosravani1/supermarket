10a58dcdbf285c6a224ca37d5190f1f534e6432d
sajadkhosravani1 <sajadkhosravani1@gmail.com>
2020-09-18 06:10:44 +0430

    Product insert operation is available by API.

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 
+app_name = 'market'
 urlpatterns = [
-
+    path('products/insert', product_insert, name='product_insert'),
 ]
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import JsonResponse,HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.db.utils import IntegrityError
+import json
+from .models import *
 
-# Create your views here.
+
+@csrf_exempt
+def product_insert(request):
+    args = json.loads(request.body.decode('utf-8'))
+    product = Product(code=args['code'], name=args['name'], price=args['price'])
+    if 'inventory' in args :
+        product.inventory = args['inventory']
+
+    try:
+        product.save()
+    except IntegrityError:
+        return JsonResponse({'message': "There's another product with this code."}, status=400)
+    except Exception as e:
+        return JsonResponse({'message': str(e)}, status=400)
+    return JsonResponse({'id':product.id}, status=201)
+
